e2fca3ce74ff96b72065f4dfefc2ee6abceff178
Joseph Morain <morain@echoworx.com>
2018-11-16 16:56:21 -0500

    translation service

---
+++ src/main/java/com/cruat/tools/translater/TranslationService.java
@@ -0,0 +1,24 @@
+package com.cruat.tools.translater;
+
+public class TranslationService {
+	public static final String BASE = "https://translation.googleapis.com";
+	//post
+	public static final String TRANSLATE = "/language/translate/v2";
+	//post
+	public static final String DETECT = "/language/translate/v2/detect";
+	//get
+	public static final String LANGUAGE = "/language/translate/v2/languages";
+	
+
+	public void detect() {
+		
+	}
+	
+	public void languages() {
+		
+	}
+	
+	public void translates() {
+		
+	}
+}
